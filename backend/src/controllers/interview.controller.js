3c89ddc334ea46cc26426d5c172f40746f6a8141
Munna -Dev <mkcodes.in@gmail.com>
2026-03-03 18:07:38 +0530

    Adding uploading file features

--- backend/src/controllers/interview.controller.js
+++ backend/src/controllers/interview.controller.js
@@ -1,15 +1,24 @@
-import pdf from 'pdf-parse'
+import { createRequire } from "module";
+import { generateInterviewReport } from "../services/ai.service.js";
+const require = createRequire(import.meta.url);
+const pdf = require("pdf-parse");
 
 export async function interviewController(req, res) {
     try {
         if (!req.file) {
             return res.status(400).json({ message: "No file uploaded" });
         }
-
+        const { selfDescription, jobDescription } = req.body;
         const data = await pdf(req.file.buffer);
 
+        const analysisContext = generateInterviewReport({
+            resume: data.text,
+            selfDescription,
+            jobDescription
+        })
+
         res.json({
-            text: data.text.trim()
+            text: analysisContext
         });
 
     } catch (error) {
